a4eb3877bcf8f06ef281dac1009c9c42d936ff59
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2018-10-26 15:31:28 -0300

    Correcciones Bluetooth

--- bluetooth_module1/bluetooth_module1.ino
+++ bluetooth_module1/bluetooth_module1.ino
@@ -1,21 +1,27 @@
 #include<SoftwareSerial.h>
 
 byte ledP = 13;
+byte Rx = 0;
+byte Tx = 1;
+
+SoftwareSerial BT1(Rx, Tx);
 
 void setup() {
-  Serial.begin(9600);
   pinMode(ledP, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Levantando el modulo Bluetooth");
+  Serial.println("Esperando comandos AT:");
+  BT1.begin(38400);
 }
 
 void loop() {
+  if(BT1.available()){
+    Serial.write(BT1.read());
+    digitalWrite(ledP, HIGH);
+  }else{
+    digitalWrite(ledP, LOW);
+  }
   if(Serial.available()){
-    char letra = Serial.read();
-    if(letra == 'a'){
-      digitalWrite(ledP, HIGH);
-      Serial.println("luz encendida");
-    }else if(letra == 'b'){
-      digitalWrite(ledP, LOW);
-      Serial.println("luz apagada");
-    }
+    BT1.write(Serial.read());
   }
 }
